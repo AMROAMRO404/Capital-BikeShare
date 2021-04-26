70feee63fd12949270b6b0d3ca523b1afc19c383
AMROAMRO404 <amro.amro1999@gmail.com>
2021-04-27 00:22:32 +0300

    set up project

---
+++ README.md
@@ -0,0 +1,5 @@
+# Capital-BikeShare-Problem
+
+data is available here: https://s3.amazonaws.com/capitalbikeshare-data/201805-capitalbikeshare-tripdata.zip
+
+please install data and add it data folder.
---
+++ src/read_data.py
@@ -0,0 +1,8 @@
+# Import pandas
+import pandas as pd
+
+# reading csv file
+df = pd.read_csv("../data/2018-05-capitalbikeshare-tripdata.csv")
+# print(df['Start station'].value_counts())
+# print(df['End station'].value_counts())
+# WE HAVE 501 STATIONS
